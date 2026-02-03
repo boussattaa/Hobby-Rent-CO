e6ac9d08ab7de01a56c2d9bdd2eeda8215f5cab0
boussattaa <159396177+boussattaa@users.noreply.github.com>
2026-02-02 17:56:12 -0700

    Fix: Restore Link import in Offroad page

--- app/offroad/page.js
+++ app/offroad/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from 'next/link';
 import { useRouter, useSearchParams } from 'next/navigation';
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
